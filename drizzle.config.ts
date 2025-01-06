8d9e2988b28516a15c920b272419adb63e7d048d
cybergaz <kkanttechy@gmail.com>
2025-01-07 03:40:20 +0530

    chore: env vars for drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'drizzle-kit'
+import "dotenv/config"
 
 // via connection params
 export default defineConfig({
     dialect: "postgresql",
     dbCredentials: {
-        host: "pgdb",
-        port: 5432,
-        user: "gaz",
-        password: "cybergaz",
-        database: "cybergaz",
+        host: process.env.POSTGRES_HOST || "pgdb",
+        port: process.env.POSTGRES_PORT ? parseInt(process.env.POSTGRES_PORT, 10) : 5432,
+        user: process.env.POSTGRES_USER || "gaz",
+        password: process.env.POSTGRES_PASSWORD || "cybergaz",
+        database: process.env.POSTGRES_DB || "cybergaz",
     },
     schema: ["./src/models/*"]
 })
